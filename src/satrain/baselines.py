e5bf825e8190edec9e9d33cea7b48fb1fb103db6
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2026-05-13 09:46:10 -0600

    Add dropdown menu to baseline notebook.

--- src/satrain/baselines.py
+++ src/satrain/baselines.py
@@ -11,12 +11,12 @@ from typing import List, Optional
 import numpy as np
 import xarray as xr
 
-from satrain.definitions import BASE_SENSORS
+from satrain.definitions import BASE_SENSORS, DOMAINS
 
 
 BASELINES_GMI = {
     "era5": "ERA5",
-    "gprof_v07": "GPROF V7 (GMI)",
+    "gprof_v07": "GPROF V07 (GMI)",
     "persiann_ccs": "PERSIANN CCS",
     "persiann_pdir_now": "PERSIANN PDIR-Now",
     "punet": "PU-Net",
@@ -26,7 +26,7 @@ BASELINES_GMI = {
 
 BASELINES_ATMS = {
     "era5": "ERA5",
-    "gprof_v07": "GPROF V7 (ATMS)",
+    "gprof_v07": "GPROF V07 (ATMS)",
     "persiann_ccs": "PERSIANN CCS",
     "persiann_pdir_now": "PERSIANN PDIR-Now",
     "punet": "PU-Net",
@@ -89,8 +89,10 @@ RETRIEVAL_TYPES = {
     "GPROF-IR": "IR",
     "PERSIANN CCS": "IR",
     "ERA5": "Reanalysis",
-    "GPROF V7 (GMI)": "PMW",
-    "GPROF V7 (ATMS)": "PMW",
+    "GPROF V07 (GMI)": "PMW",
+    "GPROF V07 (ATMS)": "PMW",
+    "GPROF V08 (GMI)": "PMW",
+    "GPROF V08 (ATMS)": "PMW",
     "PERSIANN PDIR-Now": "IR",
     "PU-Net": "IR"
 }
@@ -157,7 +159,7 @@ def get_ranked_results(
     algorithms = None
 
     for domain in ["Austria", "CONUS", "Korea"]:
-        results = load_baseline_results(base_sensor="atms", domain=domain.lower())
+        results = load_baseline_results(base_sensor=base_sensor, domain=domain.lower())
         algorithms = results.algorithm.data
         results["kind"] = (("algorithm",), [RETRIEVAL_TYPES[alg] for alg in algorithms])
         res_d = results[list(metrics) + ["kind"]].to_dataframe()
@@ -180,7 +182,6 @@ def get_ranked_results(
 
     ranks = np.array(ranks)
     algorithms = list(algorithms[np.argsort(np.nanmean(ranks, (0, 1)))])
-
     all_results = pd.concat(all_results)
 
     dframe = (
@@ -196,12 +197,23 @@ def get_ranked_results(
     all_ranks = np.array([algorithms.index(alg) for alg in dframe.algorithm])
     dframe["rank"] = all_ranks
 
-    return dframe.sort_values(by="rank")
+    return dframe.sort_values(by=["domain", "rank"])
 
 
 def plot_baselines_interactive(
-        results: "pd.Dataframe"
-):
+        results: "pd.Dataframe",
+        add_menu: bool = True,
+) -> "plotly.Figure":
+    """
+    Visualizes baseline results using plotly for interactive display on the web.
+
+    Args:
+        results: A pandas long-form Dataframe holding the results to display.
+        add_menu: If 'True', a menu is added to subset the displayed algorithms.
+
+    Return:
+        The plotly figure object.
+    """
     import numpy as np
     import plotly.express as px
     import matplotlib.pyplot as plt
@@ -232,6 +244,7 @@ def plot_baselines_interactive(
         facet_col_spacing=0.06,
         category_orders={
             "metric": metrics,
+            "domain": DOMAINS
         },
         hover_data=[
             "domain",
@@ -241,7 +254,6 @@ def plot_baselines_interactive(
         ],
         color_discrete_sequence=colors
     )
-
     fontsize = 12
     for ind, (key, name) in enumerate(metrics.items()):
         unit = UNITS.get(key, "")
@@ -255,7 +267,6 @@ def plot_baselines_interactive(
                 text=metrics[key],
                 font=dict(size=fontsize)
             )
-
     fig.for_each_annotation(format_title)
     fig.update_layout(
         legend=dict(
@@ -267,11 +278,61 @@ def plot_baselines_interactive(
             x=0.5,
         )
     )
-
     fig.update_xaxes(tickangle=45, title="Domain", title_font=dict(size=fontsize))
     fig.update_yaxes(matches=None, title_font=dict(size=fontsize))
     fig.update_layout(showlegend=True)
     fig.update_layout(
         font=dict(size=fontsize)
     )
+
+    # Menu buttons.
+    if add_menu:
+        kinds = ["All"] + sorted(results["kind"].dropna().unique())
+        buttons = []
+
+        for t in kinds:
+            if t == "All":
+                visible = [True] * len(fig.data)
+            else:
+                visible = [
+                    trace.name in results.loc[results["kind"] == t, "algorithm"].unique()
+                    for trace in fig.data
+                ]
+
+            buttons.append(
+                dict(
+                    label=t,
+                    method="update",
+                    args=[
+                        {"visible": visible},
+                        {"title": f"Algorithms: {t}"},
+                    ],
+                )
+            )
+
+        fig.add_annotation(
+            text="<b>Product Type:</b>",
+            x=0.00,
+            y=1.38,
+            xref="paper",
+            yref="paper",
+            showarrow=False,
+            align="left",
+            font=dict(size=14),
+            xanchor="left",
+            yanchor="top",
+        )
+
+        fig.update_layout(
+            updatemenus=[
+                dict(
+                    buttons=buttons,
+                    direction="down",
+                    x=0.12,
+                    y=1.4,
+                    xanchor="left",
+                    yanchor="top",
+                )
+            ]
+        )
     return fig
