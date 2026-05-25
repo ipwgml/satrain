b9f2bb9cebf6cadf7b5637ff71e101e170d848e1
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2026-05-25 13:44:12 -0600

    Fix baselines test.

--- tests/test_baselines.py
+++ tests/test_baselines.py
@@ -13,17 +13,17 @@ def test_load_baseline_results(base_sensor):
     """
     results = load_baseline_results(base_sensor)
     assert "ERA5" in results.algorithm
-    assert f"GPROF V7 ({base_sensor.upper()})" in results.algorithm
+    assert f"GPROF V07 ({base_sensor.upper()})" in results.algorithm
 
     results_austria = load_baseline_results(base_sensor, domain="austria")
     assert results["bias"].data[0] != results_austria["bias"].data[0]
     assert "ERA5" in results_austria.algorithm
-    assert f"GPROF V7 ({base_sensor.upper()})" in results_austria.algorithm
+    assert f"GPROF V07 ({base_sensor.upper()})" in results_austria.algorithm
 
     results = load_baseline_results(base_sensor, baselines=["era5"])
     assert "ERA5" in results.algorithm
-    assert f"GPROF V7 ({base_sensor.upper()})" not in results.algorithm
+    assert f"GPROF V07 ({base_sensor.upper()})" not in results.algorithm
 
     results = load_baseline_results(base_sensor, baselines=[f"gprof_v07"])
     assert "ERA5" not in results.algorithm
-    assert f"GPROF V7 ({base_sensor.upper()})" in results.algorithm
+    assert f"GPROF V07 ({base_sensor.upper()})" in results.algorithm
